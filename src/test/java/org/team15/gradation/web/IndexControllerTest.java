45eb9898f594cd438acdf8516861749db15b96b1
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-08-08 16:07:49 +0900

    8.8 IndexController 수정

--- src/test/java/org/team15/gradation/web/IndexControllerTest.java
+++ src/test/java/org/team15/gradation/web/IndexControllerTest.java
@@ -13,15 +13,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
 
-    @Test
-    public void 메인페이지_로딩(){
-        //when
-        String body = this.restTemplate.getForObject("/", String.class);
-
-        //then
-        assertThat(body).contains("Inha");
-    }
 }
