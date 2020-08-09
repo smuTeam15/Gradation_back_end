61e27554a00d9e171edfbf3569a44dddc1eccc55
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-08-09 19:56:13 +0900

    8.9 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,11 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/CallforCode/application-oauth.properties \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+##nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 #nohup java -jar \
 #  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
