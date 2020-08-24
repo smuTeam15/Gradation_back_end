7a02c5db950351dd7982baeae6863e0650759b93
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-08-24 21:37:33 +0900

    8.24 likes post 요청 수정

--- src/main/java/org/team15/gradation/domain/post/likes/PostLikesRepository.java
+++ src/main/java/org/team15/gradation/domain/post/likes/PostLikesRepository.java
@@ -3,7 +3,9 @@ package org.team15.gradation.domain.post.likes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostLikesRepository extends JpaRepository<PostLikes, Long> {
 
-    PostLikes findByPostIdAndUserId(Long postId, Long userId);
+    List<PostLikes> findByPostIdAndUserId(Long postId, Long userId);
 }
