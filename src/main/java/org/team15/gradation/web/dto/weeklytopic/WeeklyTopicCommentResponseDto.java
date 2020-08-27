8b3c4a98287736792ebffb23fa26ef9ade0aa7ad
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-08-27 22:03:03 +0900

    8.27 reponse에 commentId 추가

--- src/main/java/org/team15/gradation/web/dto/weeklytopic/WeeklyTopicCommentResponseDto.java
+++ src/main/java/org/team15/gradation/web/dto/weeklytopic/WeeklyTopicCommentResponseDto.java
@@ -3,11 +3,13 @@ package org.team15.gradation.web.dto.weeklytopic;
 import org.team15.gradation.domain.weeklytopic.comment.WeeklyTopicComment;
 
 public class WeeklyTopicCommentResponseDto {
+    private Long userId;
     private String userName;
     private Long commentId;
     private String comment;
 
     public WeeklyTopicCommentResponseDto(WeeklyTopicComment entity) {
+        this.userId = entity.getUser().getId();
         this.userName = entity.getUser().getName();
         this.commentId = entity.getId();
         this.comment = entity.getComment();
