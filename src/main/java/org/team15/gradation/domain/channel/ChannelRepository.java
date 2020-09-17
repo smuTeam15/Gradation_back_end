1a90f2f375a73705d18a34974c533055a813ee95
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-09-17 12:00:06 +0900

    9.17 쿼리 수정

--- src/main/java/org/team15/gradation/domain/channel/ChannelRepository.java
+++ src/main/java/org/team15/gradation/domain/channel/ChannelRepository.java
@@ -1,6 +1,7 @@
 package org.team15.gradation.domain.channel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -8,6 +9,7 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
     Channel findByCode(String code);
 
+    @Modifying
     @Query("update Channel c set c.first_picture = :first_picture, c.second_picture = :second_picture where c.channel_id = :channel_id")
     void updatePictureUrl(@Param("channel_id") Long id, @Param("first_picture") String firstPicture, @Param("second_picture") String secondPicture);
 }
